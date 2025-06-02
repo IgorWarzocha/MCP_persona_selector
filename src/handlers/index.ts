83f2a2a8f2a8ae6d196222d0b357f615f5548261
IgorWarzocha <igorwarzocha@gmail.com>
2025-06-02 16:21:48 +0100

    Delete src directory

--- src/handlers/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all handlers from their respective files
-export * from './filesystem-handlers.js';
-export * from './terminal-handlers.js';
-export * from './process-handlers.js';
-export * from './edit-search-handlers.js';
-// --- NEW ---
-export * from './persona_handlers.js'; // Add this line to export your new persona handlers
-// --- END NEW ---
